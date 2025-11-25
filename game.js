e07f30c938ef0f166ccf97e18c59ce9455224a8f
sato-git-hub <mimilili.disney@gmail.com>
2025-11-26 01:17:19 +0900

    ターム４の提出課題を提出

---
+++ game.js
@@ -0,0 +1,73 @@
+const result = document.getElementById("result");
+const checkBox = document.getElementById("cheatMode");
+const buttons = document.querySelectorAll(".button-group button");
+const body = document.body;
+const win = [
+  ["✊ グー", "✌️ チョキ"],
+  ["✌️ チョキ", "🖐 パー"],
+  ["🖐 パー", "✊ グー"],
+];
+
+checkBox.addEventListener("change", () => {
+  if (checkBox.checked) {
+    body.classList.add("cheat-active");
+  } else {
+    body.classList.remove("cheat-active");
+  }
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const userHand = event.target.textContent;
+    //チートモードONの場合のcpuHand
+    if (checkBox.checked) {
+      const cpuHand = cpuResult(userHand);
+      Text(userHand, cpuHand);
+    } else {
+      //cpuの手をランダムに決める
+      const randomIndex = Math.floor(Math.random() * 3);
+      const hand = ["✊ グー", "✌️ チョキ", "🖐 パー"];
+      const cpuHand = hand[randomIndex];
+      Text(userHand, cpuHand);
+    }
+  });
+});
+
+//チートモード時のcpuのhandを決定
+function cpuResult(userHand) {
+  for (let i = 0; i < win.length; i++) {
+    if (userHand === win[i][0]) {
+      return win[i][1];
+    }
+  }
+}
+
+function arrayEqual(userHand, cpuHand) {
+  return win.some((i) => {
+    return i[0] === userHand && i[1] === cpuHand;
+  });
+}
+
+//判定
+function Result(userHand, cpuHand) {
+  if (arrayEqual(userHand, cpuHand)) {
+    result.classList.remove("draw", "lose");
+    result.classList.add("win");
+    return "勝ち";
+  } else if (userHand === cpuHand) {
+    result.classList.remove("win", "lose");
+    result.classList.add("draw");
+    return "あいこ";
+  } else {
+    result.classList.remove("draw", "win");
+    result.classList.add("lose");
+    return "負け";
+  }
+}
+
+//画面表示テキスト
+function Text(userHand, cpuHand) {
+  const finalResult = Result(userHand, cpuHand);
+  const cheatStatus = checkBox.checked ? " (チートON)" : "";
+  result.innerHTML = `あなた:${userHand} 相手:${cpuHand} → ${finalResult}${cheatStatus}`;
+}
